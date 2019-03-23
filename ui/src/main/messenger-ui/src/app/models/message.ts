150567a652854aa725805186bef9c99d657ff23b
vfreiberg <vladfreiberg97@gmail.com>
2019-03-23 18:33:20 +0300

    Updated 'message' model
    * Added messageType property.
    * Added MessageType enum.

--- ui/src/main/messenger-ui/src/app/models/message.ts
+++ ui/src/main/messenger-ui/src/app/models/message.ts
@@ -1,13 +1,21 @@
 export class Message {
 
   senderUsername: string;
+  receiverUsername: string;
   messageText: string;
   timestamp: number;
+  messageType: MessageType;
 
-  constructor(senderUsername, messageText, timestamp) {
+  constructor(senderUsername = null, receiverUsername = null, messageText = null, timestamp = null) {
     this.senderUsername = senderUsername;
+    this.receiverUsername = receiverUsername;
     this.messageText = messageText;
     this.timestamp = timestamp;
   }
 
+}
+
+export enum MessageType {
+  CHAT_ACTIVITY,
+  CHAT_COMMUNICATION,
 }
